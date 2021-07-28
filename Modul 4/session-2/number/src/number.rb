54b1af39f65d25c4ae4763471008b347fef39c15
yasyars <yasya.rusyda@gmail.com>
2021-07-28 18:49:13 +0700

    Implement increment case 3,9

--- Modul 4/session-2/number/src/number.rb
+++ Modul 4/session-2/number/src/number.rb
@@ -4,9 +4,21 @@ class Number
     @array = array
   end
 
+  # def stop(temp)
+  #   temp[temp.size-1]!=9
+  # end
+
+  # def recur(temp)
+  #   temp[temp.size-1]+=1
+  # end
+
+
   def increment
     if @array[@array.size-1]!= 9
       @array[@array.size-1]+=1
+    else
+      @array[@array.size-1] = 0
+      @array[@array.size-2] +=1
     end
 
     return @array
