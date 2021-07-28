2ceb153d84703766a0f10e67f2ef054c05674b8f
yasyars <yasya.rusyda@gmail.com>
2021-07-28 18:35:17 +0700

    Initialize task

---
+++ Modul 4/session-2/number/spec/number_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe Number do
+  # it 'return 562' do
+  #   number = Number.new([5,6,1])
+
+  #   result = number.increment
+
+  #   expect(result).to eq([5,6,2])
+  # end
+
+  # it 'return 39' do
+  #   number = Number.new([])
+
+  #   result = number.increment
+
+  #   expect(result).to eq([5,6,2])
+  # end
+end
