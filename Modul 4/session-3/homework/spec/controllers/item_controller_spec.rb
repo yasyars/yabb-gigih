253ecbbc3c0559e53dd8ea887ad1f85c4790dca2
yasyars <yasya.rusyda@gmail.com>
2021-07-31 22:35:55 +0700

    Add unit test for item controller #create_item_form

--- Modul 4/session-3/homework/spec/controllers/item_controller_spec.rb
+++ Modul 4/session-3/homework/spec/controllers/item_controller_spec.rb
@@ -2,12 +2,44 @@ require_relative '../../controller/item_controller.rb'
 require './models/item'
 
 describe ItemController do
+  before [:each] do
+    client = create_db_client
+    client.query("SET FOREIGN_KEY_CHECKS = 0")
+    client.query("TRUNCATE TABLE item_categories")
+    client.query("TRUNCATE TABLE items")
+    client.query("TRUNCATE TABLE categories")
+    client.query("SET FOREIGN_KEY_CHECKS =1")
+    @category1 = Category.new({
+      id:"1",
+      name: "Main Dish"})
+    @category1.save
+    @category2 = Category.new({
+      id: "2",
+      name: "Beverage"})
+    @category2.save
+  end
+  
+  describe '#create_item_form' do
+    context 'when initialize' do
+      it 'should return view' do
+        controller = ItemController.new
+        response = controller.create_item_form
+        categories = 
+        expected_view = ERB.new(File.read("./views/create.erb")).result_with_hash(
+        {
+          categories: [@category1, @category2]
+        })
+
+        expect(response).to eq(expected_view)
+      end
+    end
+  end
+
   describe '#list_items' do
     context 'when initialized'do 
-      it 'should return item page'do
+      it 'should return item view'do
         controller = ItemController.new
         response = controller.list_items
-
         expected_view = ERB.new(File.read("./views/index.erb")).result_with_hash(
           {
             items: Item.find_all
@@ -16,9 +48,10 @@ describe ItemController do
         expect(response).to eq(expected_view)
       end
     end
-  end
+  end 
 
   describe '#update_item' do
+
     context 'when the parameter is invalid' do
       it 'returns error' do
         controller = ItemController.new
